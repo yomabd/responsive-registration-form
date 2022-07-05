ee3a0fc1d44a714056a9b2657c55e9d1d9283662
yomabd <idreesyomi@gmail.com>
2022-07-05 22:16:24 +0100

    add validation effects

--- index.js
+++ index.js
@@ -1,31 +1,41 @@
 console.log("hello, i am working");
 inputs = document.getElementsByTagName("input");
-form = document.querySelector("form");
+images = document.getElementsByTagName("img");
 submit = document.querySelector(".neutral");
-spans = document.querySelectorAll(".js");
-submit.addEventListener("mouseover", function () {
-  var i = 0;
-  for (input of inputs) {
-    if (input.value === "") {
-      input.style.outline = "1.5px solid hsl(0, 100%, 74%)";
-      input.style.color = "hsl(0, 100%, 74%)";
+spans = document.querySelectorAll(".text-below");
 
-      spans[i].style.setProperty("--aftercontent", "content after");
-      i++;
-    }
-    val = input.value;
-    if (input.getAttribute("type") === "email") {
-      for (i of val) {
-        if (i === "/") {
-          spans[2].style.setProperty("--aftercontent", "email looks incorrect");
+// function definition
+function validation() {
+  for (let i = 0; i < 4; i++) {
+    if (i === 2) {
+      for (l of inputs[i].value) {
+        if (l === "/") {
+          inputs[i].style.outline = "1.5px solid hsl(0, 100%, 74%)";
+          inputs[i].style.color = "hsl(0, 100%, 74%)";
+          images[i].style.setProperty("display", "block");
+          spans[i].textContent = "looks like this is not an email";
+          continue;
         }
       }
     }
+    if (inputs[i].value === "") {
+      inputs[i].style.outline = "1.5px solid hsl(0, 100%, 74%)";
+      images[i].style.setProperty("display", "block");
+      spans[i].textContent =
+        inputs[i].getAttribute("placeholder") + " cannot be empty";
+    }
   }
-});
+}
+
+submit.addEventListener("mouseover", validation);
 submit.addEventListener("mouseout", function () {
+  i = 0;
   for (input of inputs) {
     input.style.outline = "1.5px solid black";
     input.style.color = "black";
+    spans[i].textContent = "";
+    images[i].style.setProperty("display", "none");
+
+    i++;
   }
 });
